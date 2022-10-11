30bb08235a6d5f666df28f5cf09785defe6c36e9
rodox-karius <rcarius@gmail.com>
2022-10-11 00:43:22 -0300

    Commit repositorios

---
+++ src/main/java/br/com/residencia/Cinema/repository/DiretorRepository.java
@@ -0,0 +1,9 @@
+package br.com.residencia.Cinema.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.residencia.Cinema.entity.Diretor;
+
+public interface DiretorRepository extends JpaRepository<Diretor , Integer>{
+
+}
---
+++ src/main/java/br/com/residencia/Cinema/repository/GeneroRepository.java
@@ -0,0 +1,9 @@
+package br.com.residencia.Cinema.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.residencia.Cinema.entity.Genero;
+
+public interface GeneroRepository extends JpaRepository<Genero , Integer>{
+
+}
